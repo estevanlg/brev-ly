bb413d17273da8e9e086a53a7bb77d00ec7bc15e
Estevan Luis <lg.estevan@gmail.com>
2026-09-16 22:08:45 -0300

    swagger config

---
+++ src/infra/http/routes/links.ts
@@ -0,0 +1,30 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+export const registerLinksRoutes: FastifyPluginAsyncZod = async server => {
+	server.post(
+		'/links',
+		{
+			schema: {
+				summary: 'Create a new link',
+				body: z.object({
+					originalUrl: z.url(),
+					shortenedUrl: z.url(),
+				}),
+				response: {
+					201: z.object({
+						id: z.string(),
+					}),
+					409: z
+						.object({
+							message: z.string(),
+						})
+						.describe('Link already exists.'),
+				},
+			},
+		},
+		async (request, reply) => {
+			return reply.status(201).send({ id: 'some-generated-id' })
+		}
+	)
+}
--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,9 +1,48 @@
 import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
+import {
+	hasZodFastifySchemaValidationErrors,
+	jsonSchemaTransform,
+	serializerCompiler,
+	validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { registerLinksRoutes } from './routes/links'
 
 const server = fastify()
 
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+	if (hasZodFastifySchemaValidationErrors(error)) {
+		return reply.status(400).send({
+			message: 'Validation Error',
+			issues: error.validation,
+		})
+	}
+
+	console.error(error)
+	return reply.status(500).send({ message: 'Internal Server Error' })
+})
+
 server.register(fastifyCors, { origin: '*' })
+
+server.register(fastifySwagger, {
+	openapi: {
+		info: {
+			title: 'Brev.ly API Documentation',
+			version: '1.0.0',
+		},
+	},
+	transform: jsonSchemaTransform,
+})
+server.register(fastifySwaggerUi, {
+	routePrefix: '/docs',
+})
+
+server.register(registerLinksRoutes)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
 	console.log('HTTP server running!')
