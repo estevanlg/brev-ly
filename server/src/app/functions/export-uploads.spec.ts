5161ac60eb0a5bea83009f23e229bcf203c8f8b1
Estevan Luis <lg.estevan@gmail.com>
2026-09-20 13:59:28 -0300

    test config

--- server/src/app/functions/export-uploads.spec.ts
+++ server/src/app/functions/export-uploads.spec.ts
@@ -1,60 +1,58 @@
-import { randomUUID } from 'node:crypto'
-import { describe, expect, it, vi } from 'vitest'
-import { exportUploads } from '@/app/functions/export-uploads'
-import { db } from '@/infra/db'
-import { schema } from '@/infra/db/schemas'
-import { isRight, unwrapEither } from '@/infra/shared/either'
-import * as upload from '@/infra/storage/upload-file-to-storage'
+import { randomUUID } from "node:crypto";
+import { describe, expect, it, vi } from "vitest";
+import { exportUploads } from "@/app/functions/export-uploads";
+import { db } from "@/infra/db";
+import { schema } from "@/infra/db/schemas";
+import { isRight, unwrapEither } from "@/infra/shared/either";
+import * as upload from "@/infra/storage/upload-file-to-storage";
 
-describe('export uploads', () => {
-	it('should be able to export uploads', async () => {
-		const uploadStub = vi
-			.spyOn(upload, 'uploadFileToStorage')
-			.mockImplementationOnce(async () => {
-				return {
-					key: `${randomUUID()}.csv`,
-					url: 'http://example.com/file.csv',
-				}
-			})
+describe("export uploads", () => {
+  it("should be able to export uploads", async () => {
+    const uploadStub = vi
+      .spyOn(upload, "uploadFileToStorage")
+      .mockImplementationOnce(async () => {
+        return {
+          key: `${randomUUID()}.csv`,
+          url: "http://example.com/file.csv",
+        };
+      });
 
-		const sut = await exportUploads()
+    const sut = await exportUploads();
 
-		const generatedCSVStream = uploadStub.mock.calls[0][0].contentStream
-		const csvAsString = await new Promise<string>((resolve, reject) => {
-			const chunks: Buffer[] = []
+    const generatedCSVStream = uploadStub.mock.calls[0][0].contentStream;
+    const csvAsString = await new Promise<string>((resolve, reject) => {
+      const chunks: Buffer[] = [];
 
-			generatedCSVStream.on('data', (chunk: Buffer) => {
-				chunks.push(chunk)
-			})
+      generatedCSVStream.on("data", (chunk: Buffer) => {
+        chunks.push(chunk);
+      });
 
-			generatedCSVStream.on('end', () => {
-				resolve(Buffer.concat(chunks).toString('utf-8'))
-			})
+      generatedCSVStream.on("end", () => {
+        resolve(Buffer.concat(chunks).toString("utf-8"));
+      });
 
-			generatedCSVStream.on('error', err => {
-				reject(err)
-			})
-		})
+      generatedCSVStream.on("error", (err) => {
+        reject(err);
+      });
+    });
 
-		const csvAsArray = csvAsString
-			.trim()
-			.split('\n')
-			.map(row => row.split(','))
+    const csvAsArray = csvAsString
+      .trim()
+      .split("\n")
+      .map((row) => row.split(","));
 
-		expect(isRight(sut)).toBe(true)
-		expect(unwrapEither(sut).reportUrl).toBe('http://example.com/file.csv')
+    expect(isRight(sut)).toBe(true);
+    expect(unwrapEither(sut).reportUrl).toBe("http://example.com/file.csv");
 
-		console.log('csvAsArray', csvAsArray)
+    const result = await db.select().from(schema.links);
 
-		const result = await db.select().from(schema.links)
-
-		for (const r of result) {
-			expect(csvAsArray).toContainEqual([
-				r.originalUrl,
-				r.shortenedUrl,
-				String(r.visitorCounter),
-				expect.any(String),
-			])
-		}
-	})
-})
+    for (const r of result) {
+      expect(csvAsArray).toContainEqual([
+        r.originalUrl,
+        r.shortenedUrl,
+        String(r.visitorCounter),
+        expect.any(String),
+      ]);
+    }
+  });
+});
