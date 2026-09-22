12953eeca33789deb0ef947d71c66fde53ae0d2f
Estevan Luis <lg.estevan@gmail.com>
2026-09-22 20:21:26 -0300

    layout ajusts

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,15 +1,23 @@
 import { LinkForm } from "./components/link-form";
 import { ListLinks } from "./components/list-links";
-import logoIgnite from "./assets/Logo.svg";
+import brevlyLogo from "./assets/Logo.svg";
 
 export function App() {
   return (
-    <main className="flex flex-col lg:flex-row gap-6 justify-center items-start h-auto lg:h-dvh px-4">
-      <div className="flex flex-col items-center lg:items-start gap-6">
-        <img src={logoIgnite} alt="Logo Ignite" className="h-8 text-blue-base mx-auto lg:mx-0" />
-        <LinkForm />
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center lg:items-start w-full max-w-5xl p-4">
+        <img src={brevlyLogo} alt="Logo Brevly" className="text-blue-base h-6 mb-2" />
+        
+        <div className="flex flex-col lg:flex-row gap-6 w-full">
+          <div className="flex-none">
+            <LinkForm />
+          </div>
+          
+          <div className="flex-1">
+            <ListLinks />
+          </div>
+        </div>
       </div>
-      <ListLinks />
     </main>
   )
 }
