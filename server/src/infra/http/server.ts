00b96bbc7d80256a2845a46640c697436c86803b
Estevan Luis <lg.estevan@gmail.com>
2026-09-23 23:05:43 -0300

    cors error on delete api

--- server/src/infra/http/server.ts
+++ server/src/infra/http/server.ts
@@ -1,57 +1,61 @@
-import { fastifyCors } from '@fastify/cors'
-import { fastifySwagger } from '@fastify/swagger'
-import { fastifySwaggerUi } from '@fastify/swagger-ui'
-import { fastify } from 'fastify'
+import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import { fastify } from "fastify";
 import {
-	hasZodFastifySchemaValidationErrors,
-	jsonSchemaTransform,
-	serializerCompiler,
-	validatorCompiler,
-} from 'fastify-type-provider-zod'
-import { createLinkRoute } from './routes/create-link'
-import { deleteLinkRoute } from './routes/delete-link'
-import { exportLinksRoute } from './routes/export-links'
-import { getLinksRoute } from './routes/get-links'
-import { updateVisitorCounterRoute } from './routes/update-link'
+  hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { createLinkRoute } from "./routes/create-link";
+import { deleteLinkRoute } from "./routes/delete-link";
+import { exportLinksRoute } from "./routes/export-links";
+import { getLinksRoute } from "./routes/get-links";
+import { updateVisitorCounterRoute } from "./routes/update-link";
 
-const server = fastify()
+const server = fastify();
 
-server.setValidatorCompiler(validatorCompiler)
-server.setSerializerCompiler(serializerCompiler)
+server.setValidatorCompiler(validatorCompiler);
+server.setSerializerCompiler(serializerCompiler);
 
 server.setErrorHandler((error, _request, reply) => {
-	if (hasZodFastifySchemaValidationErrors(error)) {
-		return reply.status(400).send({
-			message: 'Validation Error',
-			issues: error.validation,
-		})
-	}
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: "Validation Error",
+      issues: error.validation,
+    });
+  }
 
-	console.error(error)
-	return reply.status(500).send({ message: 'Internal Server Error' })
-})
+  console.error(error);
+  return reply.status(500).send({ message: "Internal Server Error" });
+});
 
-server.register(fastifyCors, { origin: '*' })
+server.register(fastifyCors, {
+  origin: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+});
 
 server.register(fastifySwagger, {
-	openapi: {
-		info: {
-			title: 'Brev.ly API Documentation',
-			version: '1.0.0',
-		},
-	},
-	transform: jsonSchemaTransform,
-})
+  openapi: {
+    info: {
+      title: "Brev.ly API Documentation",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
 server.register(fastifySwaggerUi, {
-	routePrefix: '/docs',
-})
+  routePrefix: "/docs",
+});
 
-server.register(createLinkRoute)
-server.register(getLinksRoute)
-server.register(deleteLinkRoute)
-server.register(updateVisitorCounterRoute)
-server.register(exportLinksRoute)
+server.register(createLinkRoute);
+server.register(getLinksRoute);
+server.register(deleteLinkRoute);
+server.register(updateVisitorCounterRoute);
+server.register(exportLinksRoute);
 
-server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
-	console.log('HTTP server running!')
-})
+server.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
+  console.log("HTTP server running!");
+});
